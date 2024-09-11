c85d64a260445c817b848005de4179beecbcfde4
Muhammad Affandes <affandes@uin-suska.ac.id>
2024-09-11 09:29:29 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String teks = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        String[] katakata = teks.split(" ");
+        System.out.println(katakata.length);
+
+        for (int i = 0; i < katakata.length; i++) {
+            System.out.println(katakata[i]);
+        }
+
+    }
+
+
+
+}
